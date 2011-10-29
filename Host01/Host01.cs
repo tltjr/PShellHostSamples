48be195daa4854a7aaa7847e0c55d7bb39ba7226
Thomas Thornton <thomas.thornton.jr@gmail.com>
2011-10-29 12:25:13 -0700

    1-3

---
+++ Host01/Host01.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace Host01
+{
+    /// <summary>
+    /// This class contains the Main entry point for this host application.
+    /// </summary>
+    internal class Host01
+    {
+
+        /// <summary>
+        /// Indicator to tell the host application that it should exit.
+        /// </summary>
+        private bool shouldExit;
+
+        /// <summary>
+        /// The exit code that the host application will use to exit.
+        /// </summary>
+        private int exitCode;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the 
+        /// host application should exit.
+        /// </summary>
+        public bool ShouldExit
+        {
+            get { return this.shouldExit; }
+            set { this.shouldExit = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the PSHost implementation that is
+        /// used to tell the host application what code to use
+        /// when exiting.
+        /// </summary>
+        public int ExitCode
+        {
+            get { return this.exitCode; }
+            set { this.exitCode = value; }
+        }
+
+        /// <summary>
+        /// This sample uses a PowerShell object to run
+        /// a script that calls exit. The host application looks at
+        /// this and prints out the result.
+        /// </summary>
+        /// <param name="args">Parameter not used.</param>
+        private static void Main(string[] args)
+        {
+            // Create an instance of this host application class so that 
+            // the Windows PowerShell engine will have access to the 
+            // ShouldExit and ExitCode parameters.
+            Host01 me = new Host01();
+
+            // Create the host instance to use.
+            MyHost myHost = new MyHost(me);
+
+            // Create a runspace that uses the host object and run the 
+            // script using a PowerShell object.
+            using (Runspace myRunSpace = RunspaceFactory.CreateRunspace(myHost))
+            {
+                // Open the runspace.
+                myRunSpace.Open();
+
+                // Create a PowerShell object to run the script.
+                using (PowerShell powershell = PowerShell.Create())
+                {
+                    powershell.Runspace = myRunSpace;
+
+                    // Create the pipeline and run the script 
+                    // "exit (2+2)".
+                    string script = "exit (2+2)";
+                    powershell.AddScript(script);
+                    powershell.Invoke(script);
+                }
+
+                // Check the flags and see if they were set propertly.
+                Console.WriteLine(
+                    "ShouldExit={0} (should be True); ExitCode={1} (should be 4)",
+                    me.ShouldExit, 
+                    me.ExitCode);
+
+                // close the runspace to free resources.
+                myRunSpace.Close();
+            }
+
+            Console.WriteLine("Hit any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Host02/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Globalization;
+
+namespace Host02
+{
+    class Host02
+  {
+    /// <summary>
+    /// Gets or sets a Boolean value that the PSHost implementation 
+    /// uses to indicate that the host application should exit.
+    /// </summary>
+    public bool ShouldExit
+    {
+      get { return shouldExit; }
+      set { shouldExit = value; }
+    }
+    private bool shouldExit;
+    
+    /// <summary>
+    /// Gets or sets a value that the PSHost implementation uses 
+    /// this property to indicate which code the host application 
+    /// should use when exiting.
+    /// </summary>
+    public int ExitCode
+    {
+      get { return exitCode; }
+      set { exitCode = value; }
+    }
+    private int exitCode;
+    
+    /// <summary>
+    /// This sample uses a PowerShell object with a host
+    /// implementation to call the Get-Process cmdlet and 
+    /// display the results as you would see them when using 
+    /// pwrsh.exe.
+    /// </summary>
+    /// <param name="args">Not used.</param>
+    static void Main(string[] args)
+    {
+      // Set the current culture to German. We want this to be picked up when the MyHost
+      // instance is created.
+      System.Threading.Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-de");
+    
+      // Create a runspace that uses the host object.
+      MyHost myHost = new MyHost(new Host02());
+      using (Runspace myRunSpace = RunspaceFactory.CreateRunspace(myHost))
+      {
+        myRunSpace.Open();
+      
+        // Create a PowerShell object to run the commands.
+        using (PowerShell powershell = PowerShell.Create())
+        {
+          powershell.Runspace = myRunSpace;
+        
+          // Add the script to run. This script does two things. The  
+          // Get-Process cmdlet is piped to sort (alias for Sort-Object) 
+          // to display the processes by their handle count, and then 
+          // the Get-Date cmdlet is piped to Out-String so that the date 
+          // date is displayed German.
+        
+          powershell.AddScript(@"
+                               get-process | sort handlecount
+                               # This should display the date in German.
+                               get-date | out-string");
+          
+          // Add the default outputter to the end of the pipe and then call the 
+          // MergeMyResults method to merge the output and error streams from the 
+          // pipeline. This will result in the output being written using the PSHost
+          // and PSHostUserInterface classes instead of returning objects to the host
+          // application.
+        
+          powershell.AddCommand("out-default");
+          powershell.Commands.Commands[0].MergeMyResults(PipelineResultTypes.Error, PipelineResultTypes.Output);
+        
+          // Invoke the commands in the pipeline. Nothing more is needed because 
+          // no objects are returned. They are all consumed by the Out-Default 
+          // cmdlet.
+
+          powershell.Invoke();
+        }
+      }
+    
+      System.Console.WriteLine("Hit any key to exit...");
+      System.Console.ReadKey();
+    }
+  }
+}
